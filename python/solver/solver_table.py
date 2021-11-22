b4d5972908904738b5ea6a0d55df1ba3205cb659
JavierMJK <javier.garcia@xyleminc.com>
2021-11-22 15:48:22 +0100

    added table creation method

--- python/solver/solver_table.py
+++ python/solver/solver_table.py
@@ -1,7 +1,8 @@
 # https://realpython.com/qt-designer-python/
 # 
-# To convert gui from qt designer to a python file: "pyuic5 -x .\main_window.ui -o main_window.py"
+# To convert gui from qt designer to a python file: "pyuic5 -x .\main_window_table.ui -o main_window_table.py"
 
+from typing import Text
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from main_window_table import Ui_MainWindow
@@ -13,39 +14,41 @@ class Solver():
         self.ui = ui
         self.ui.lineEdit_m.setText('#')
         self.ui.lineEdit_n.setText('#') 
-        self.ui.lineEdit_x0.textChanged.connect(lambda: self.update_field('x0'))    
-        self.ui.lineEdit_x1.textChanged.connect(lambda: self.update_field('x1'))
-        self.ui.lineEdit_y0.textChanged.connect(lambda: self.update_field('y0'))
-        self.ui.lineEdit_y1.textChanged.connect(lambda: self.update_field('y1'))
+        # self.ui.lineEdit_x0.textChanged.connect(lambda: self.update_field('x0'))    
+        # self.ui.lineEdit_x1.textChanged.connect(lambda: self.update_field('x1'))
+        # self.ui.lineEdit_y0.textChanged.connect(lambda: self.update_field('y0'))
+        # self.ui.lineEdit_y1.textChanged.connect(lambda: self.update_field('y1'))
         self.ui.lineEdit_x.textChanged.connect(lambda: self.update_field('x'))
         self.ui.lineEdit_y.textChanged.connect(lambda: self.update_field('y'))
+        self.ui.pushButton_create_table.clicked.connect(self.create_table)
+        self.n_points = 0
         self.lin_solver = LinSolver()
     
     def update_field(self, field):
-        try:
-            if field == 'x0':
-                x0 = float(self.ui.lineEdit_x0.text())
-                m,n = self.lin_solver.set_x0(x0)
-                self.ui.lineEdit_m.setText(str(m))
-                self.ui.lineEdit_n.setText(str(n))
-            elif field == 'x1':
-                x1 = float(self.ui.lineEdit_x1.text())
-                m,n = self.lin_solver.set_x1(x1)
-                self.ui.lineEdit_m.setText(str(m))
-                self.ui.lineEdit_n.setText(str(n))
-            elif field == 'y0':
-                y0 = float(self.ui.lineEdit_y0.text())
-                m,n = self.lin_solver.set_y0(y0)
-                self.ui.lineEdit_m.setText(str(m))
-                self.ui.lineEdit_n.setText(str(n))        
-            elif field == 'y1':
-                y1 = float(self.ui.lineEdit_y1.text())
-                m,n = self.lin_solver.set_y1(y1)
-                self.ui.lineEdit_m.setText(str(m))
-                self.ui.lineEdit_n.setText(str(n))
-        except:
-            self.ui.lineEdit_m.setText('#')
-            self.ui.lineEdit_n.setText('#')  
+        # try:
+        #     if field == 'x0':
+        #         x0 = float(self.ui.lineEdit_x0.text())
+        #         m,n = self.lin_solver.set_x0(x0)
+        #         self.ui.lineEdit_m.setText(str(m))
+        #         self.ui.lineEdit_n.setText(str(n))
+        #     elif field == 'x1':
+        #         x1 = float(self.ui.lineEdit_x1.text())
+        #         m,n = self.lin_solver.set_x1(x1)
+        #         self.ui.lineEdit_m.setText(str(m))
+        #         self.ui.lineEdit_n.setText(str(n))
+        #     elif field == 'y0':
+        #         y0 = float(self.ui.lineEdit_y0.text())
+        #         m,n = self.lin_solver.set_y0(y0)
+        #         self.ui.lineEdit_m.setText(str(m))
+        #         self.ui.lineEdit_n.setText(str(n))        
+        #     elif field == 'y1':
+        #         y1 = float(self.ui.lineEdit_y1.text())
+        #         m,n = self.lin_solver.set_y1(y1)
+        #         self.ui.lineEdit_m.setText(str(m))
+        #         self.ui.lineEdit_n.setText(str(n))
+        # except:
+        #     self.ui.lineEdit_m.setText('#')
+        #     self.ui.lineEdit_n.setText('#')  
         if field == 'x':
             try:
                 x = float(self.ui.lineEdit_x.text())
@@ -65,6 +68,13 @@ class Solver():
         ui_obj.blockSignals(True)
         ui_obj.setText(text)
         ui_obj.blockSignals(False)
+    
+    def create_table(self):
+        try:
+            self.n_points = int(self.ui.lineEdit_n_points.text())
+            self.ui.tableWidget_points.setRowCount(self.n_points)
+        except:
+            return
 
        
 
